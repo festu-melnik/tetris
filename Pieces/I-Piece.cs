7c84b14ceddc39c6e6d7d4829678bbc802709525
melnik <mr.a.k.m@yandex.ru>
2013-09-12 15:53:02 +1100

    update: add new figure

---
+++ Pieces/I-Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisGame.Pieces
+{
+    class I_Piece : Piece
+    {
+        public I_Piece()
+        {
+            form = new XY[2][]
+            {
+                new XY [4] {new XY(1, 0), new XY(1, -1), new XY(1, -2), new XY(1, -3)},
+                new XY [4] {new XY(0, -1), new XY(1, -1), new XY(2, -1), new XY(3, -1)}
+            };
+            type = 3;
+        }
+    }
+}
